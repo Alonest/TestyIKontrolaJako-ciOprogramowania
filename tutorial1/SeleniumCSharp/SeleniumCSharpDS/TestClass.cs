07c4c98d7ab035c9e4d872164bd395ac1594dc02
Alonest <61881447+Alonest@users.noreply.github.com>
2022-06-19 18:47:09 +0200

    Add files via upload

---
+++ tutorial1/SeleniumCSharp/SeleniumCSharpDS/TestClass.cs
@@ -0,0 +1,42 @@
+﻿// NUnit 3 tests
+// See documentation : https://github.com/nunit/docs/wiki/NUnit-Documentation
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Chrome;
+using SeleniumCSharpDS.BaseClasss;
+using System.Threading;
+
+namespace SeleniumCSharpDS
+{
+    [TestFixture]
+    public class TestClass : BaseTest
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            IWebElement emailTextField=driver.FindElement(By.XPath(".//*[@id='email']"));
+            emailTextField.SendKeys("Selenum C#");
+        }
+
+
+        [Test]
+        public void TestMethod2()
+        {
+            IWebElement emailTextField = driver.FindElement(By.XPath(".//*[@id='email']"));
+            emailTextField.SendKeys("Selenum C#");
+        }
+
+        [Test]
+        public void TestMethod3()
+        {
+            IWebElement emailTextField = driver.FindElement(By.XPath(".//*[@id='email']"));
+            emailTextField.SendKeys("Selenum C#");
+            Thread.Sleep(5000);
+        }
+    }
+}
